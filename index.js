04ba047553dbd9b1faa36c9f1349ed568adf5254
chankruze <chankruze@gmail.com>
2020-06-27 02:49:39 +0530

    Pata Nahi Behenchod CORS gand maar rha
    Signed-off-by: chankruze <chankruze@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const cors = require('cors');
+// const cors = require('cors');
 const PORT = process.env.PORT || 5050;
 
 const app = express();
@@ -13,6 +13,9 @@ connectDB();
 // Log the requests
 app.use((req, res, next) => {
     console.log(`${new Date().toString()}   ${req.method}   ${req.host}   ${req.hostname}   ${req.url}`);
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 })
 
@@ -23,22 +26,22 @@ app.use(express.json({ extented: false }));
 // Define Routes
 app.use('/cap', require('./routes/cap'));
 
-let allowedOrigins = [`http://localhost:${PORT}`,
-    `http://127.0.0.1:${PORT}`,
-    'https://giveaway.geekofia.in'];
+// let allowedOrigins = [`http://localhost:${PORT}`,
+// `http://127.0.0.1:${PORT}`,
+//     'https://giveaway.geekofia.in'];
 
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin 
-        // (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}));
+// app.use(cors({
+//     origin: function (origin, callback) {
+//         // allow requests with no origin 
+//         // (like mobile apps or curl requests)
+//         if (!origin) return callback(null, true);
+//         if (allowedOrigins.indexOf(origin) === -1) {
+//             var msg = 'The CORS policy for this site does not ' +
+//                 'allow access from the specified Origin.';
+//             return callback(new Error(msg), false);
+//         }
+//         return callback(null, true);
+//     }
+// }));
 
 app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
