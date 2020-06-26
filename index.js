b380555ffd9dad12fb06da4e35106be95c7322fc
chankruze <chankruze@gmail.com>
2020-06-27 01:32:42 +0530

    Fix The Cors Error
    Signed-off-by: chankruze <chankruze@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
@@ -20,4 +21,22 @@ app.use(express.json({ extented: false }));
 app.use('/cap', require('./routes/cap'));
 
 const PORT = process.env.PORT || 5050;
+
+let allowedOrigins = [`http://localhost:${PORT}`,
+    'https://giveaway.geekofia.in'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+
 app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
