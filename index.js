62c2976bdf9ce052c8b0cd7804b10c07fd0bdbfe
chankruze <chankruze@gmail.com>
2020-06-27 02:14:58 +0530

    Fixing CORS for geekofia domain
    Signed-off-by: chankruze <chankruze@gmail.com>

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const PORT = process.env.PORT || 5050;
 
 const app = express();
 
 // connect to database
 connectDB();
 
-app.use(cors());
+// app.use(cors());
 
 // Log the requests
 app.use((req, res, next) => {
@@ -22,23 +23,22 @@ app.use(express.json({ extented: false }));
 // Define Routes
 app.use('/cap', require('./routes/cap'));
 
+let allowedOrigins = [`http://localhost:${PORT}`,
+    `http://127.0.0.1:${PORT}`,
+    'https://giveaway.geekofia.in'];
 
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
-// let allowedOrigins = [`http://localhost:${PORT}`,
-//     'https://giveaway.geekofia.in'];
-
-// app.use(cors({
-//     origin: function (origin, callback) {
-//         // allow requests with no origin 
-//         // (like mobile apps or curl requests)
-//         if (!origin) return callback(null, true);
-//         if (allowedOrigins.indexOf(origin) === -1) {
-//             var msg = 'The CORS policy for this site does not ' +
-//                 'allow access from the specified Origin.';
-//             return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//     }
-// }));
-const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
